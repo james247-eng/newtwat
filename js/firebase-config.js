e3e1893902b117282265262743fda124aaceeb97
james247-eng <danieljamesojoagefu4@gmail.com>
2026-02-05 20:37:59 +0100

    #

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
+// Firebase Configuration - Client Side Only
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-import { getFunctions } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-functions.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAUUPlYnAOAzT79hMRvza6jpeI0YmrVKJQ",
@@ -13,11 +13,12 @@ const firebaseConfig = {
   measurementId: "G-2HXV5FW483"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const functions = getFunctions(app);
 
+// Auth state observer
 let currentUser = null;
 
 onAuthStateChanged(auth, (user) => {
@@ -33,4 +34,5 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-export { auth, db, functions, currentUser };
+// REMOVED: export { functions } - We don't use Firebase Functions anymore
+export { auth, db, currentUser };
