502cd1c33002c669e60ffd7b79bbf93d4b53d585
james247-eng <danieljamesojoagefu4@gmail.com>
2026-02-05 23:23:38 +0100

    # I CHANGED THE ELEVEN LABS VERSION FROM MONOLINGUL TO MULTI LINGUAL V2

--- api/generate-tts.js
+++ api/generate-tts.js
@@ -233,7 +233,7 @@ module.exports = async (req, res) => {
       `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
       {
         text: text,
-        model_id: 'eleven_monolingual_v1',
+        model_id: 'eleven_multilingual_v2',
         voice_settings: settings || {
           stability: 0.5,
           similarity_boost: 0.75,
